3d324540b18ea0dd02d142226309151b8c46a230
RubenSandesh <133231514+RubenSandesh@users.noreply.github.com>
2023-05-12 17:34:40 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,36 @@
+class TestClass {
+    public static void main(String[] args) {
+       System.out.println(calculateNextNum(19));
+    }
+    
+    private static int calculateNextNum(int fetchedValue) {
+        int nextNo = fetchedValue + 1;
+        while (!hasDesiredSum(nextNo)) {
+            nextNo++;
+        }
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return nextNo;
+    }
+
+    private static  boolean hasDesiredSum(int number) {
+        int sum = calculateSum(number);
+        while (sum > 9) {
+            sum = calculateSum(sum);
+        }
+        return sum == 1;
+    }
+
+    private static int calculateSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+}
